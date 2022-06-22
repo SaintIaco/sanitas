95ac503890a6225a1a72c1c152dc1daf5858f580
BABEL.sduran <BABEL.sduran@iberia.es>
2022-06-22 11:11:11 +0200

    fifth-commit: develop of CalculatorCommand.java

--- src/main/java/com/sanitas/calculator/operations/SubtractionOperation.java
+++ src/main/java/com/sanitas/calculator/operations/SubtractionOperation.java
@@ -1,4 +1,12 @@
 package com.sanitas.calculator.operations;
 
+import com.sanitas.calculator.model.vo.request.CalculatorWebRequest;
+import com.sanitas.calculator.model.vo.response.CalculatorWebResponse;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SubtractionOperation {
+    public CalculatorWebResponse subtrac(CalculatorWebRequest webRequest) {
+        return null;
+    }
 }
--- src/main/java/com/sanitas/calculator/ports/primary/CalculatorCommand.java
+++ src/main/java/com/sanitas/calculator/ports/primary/CalculatorCommand.java
@@ -1,12 +1,30 @@
 package com.sanitas.calculator.ports.primary;
 
 import com.sanitas.calculator.model.vo.request.CalculatorWebRequest;
+import com.sanitas.calculator.model.vo.request.Operator;
 import com.sanitas.calculator.model.vo.response.CalculatorWebResponse;
+import com.sanitas.calculator.operations.AdditionOperation;
+import com.sanitas.calculator.operations.SubtractionOperation;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorCommand {
+
+    private final AdditionOperation additionOperation;
+    private final SubtractionOperation subtractionOperation;
+
+    public CalculatorCommand(AdditionOperation additionOperation, SubtractionOperation subtractionOperation) {
+        this.additionOperation = additionOperation;
+        this.subtractionOperation = subtractionOperation;
+    }
+
     public CalculatorWebResponse operate(CalculatorWebRequest webRequest) {
-        return null;
+        CalculatorWebResponse webResponse = null;
+        if(webRequest.getOperator().equals(Operator.ADDITION)){
+            webResponse = additionOperation.add(webRequest);
+        } else if(webRequest.getOperator().equals(Operator.SUBTRACTION)){
+            webResponse = subtractionOperation.subtrac(webRequest);
+        }
+        return webResponse;
     }
 }
