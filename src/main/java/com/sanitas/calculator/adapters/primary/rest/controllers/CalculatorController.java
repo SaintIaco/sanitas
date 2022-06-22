2768c393399166de515b9f086dba09113c108360
BABEL.sduran <BABEL.sduran@iberia.es>
2022-06-22 10:49:03 +0200

    fourth-commit: develop of CalculatorController.java

--- src/main/java/com/sanitas/calculator/adapters/primary/rest/controllers/CalculatorController.java
+++ src/main/java/com/sanitas/calculator/adapters/primary/rest/controllers/CalculatorController.java
@@ -1,4 +1,26 @@
 package com.sanitas.calculator.adapters.primary.rest.controllers;
 
+import com.sanitas.calculator.model.vo.request.CalculatorWebRequest;
+import com.sanitas.calculator.model.vo.response.CalculatorWebResponse;
+import com.sanitas.calculator.ports.primary.CalculatorCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sanitas/calculator")
 public class CalculatorController {
+
+    @Autowired
+    private CalculatorCommand calculatorCommand;
+
+    @PostMapping
+    public ResponseEntity<CalculatorWebResponse> operate(@RequestBody CalculatorWebRequest webRequest){
+        final CalculatorWebResponse webResponse = calculatorCommand.operate(webRequest);
+        return new ResponseEntity<>(webResponse, HttpStatus.OK);
+    }
 }
--- src/main/java/com/sanitas/calculator/ports/primary/CalculatorCommand.java
+++ src/main/java/com/sanitas/calculator/ports/primary/CalculatorCommand.java
@@ -1,4 +1,12 @@
 package com.sanitas.calculator.ports.primary;
 
+import com.sanitas.calculator.model.vo.request.CalculatorWebRequest;
+import com.sanitas.calculator.model.vo.response.CalculatorWebResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorCommand {
+    public CalculatorWebResponse operate(CalculatorWebRequest webRequest) {
+        return null;
+    }
 }
